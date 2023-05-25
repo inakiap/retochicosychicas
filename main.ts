d283b18c4a12cddb170fa511088bb90466569510
Iñaki Alfaro <inakiap@gmail.com>
2023-05-25 21:26:35 +0200

    motores

--- main.ts
+++ main.ts
@@ -1,36 +1,79 @@
+let chicas = 0
+let mensaje = ""
+let chicos = 0
 input.onButtonPressed(Button.A, function () {
     chicas += 1
+    basic.showLeds(`
+        . # # . #
+        . # # # .
+        # . # . .
+        . # # # .
+        # # . # #
+        `)
+    edubitMotors.runMotor(MotorChannel.M1, MotorDirection.Forward, 138)
+    edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
 })
 input.onButtonPressed(Button.AB, function () {
-    mensaje = "Total " + chicas + " Chicas " + "" + " Chicos " + chicos + (chicas + chicos)
+    mensaje = "Chicas " + chicas + "" + " Chicos " + chicos + " Total " + (chicas + chicos)
     basic.showString(mensaje)
+    chicas = 0
+    chicos = 0
 })
 input.onButtonPressed(Button.B, function () {
     chicos += 1
+    basic.showLeds(`
+        # . # . .
+        . # # # .
+        . . # . #
+        . # . # .
+        # . . . #
+        `)
+    edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    edubitMotors.brakeMotor(MotorChannel.M1)
 })
-let chicos = 0
-let mensaje = ""
-let chicas = 0
-basic.showLeds(`
-    . # . # .
-    . . . . .
-    # . . . #
-    # . . . #
-    . # # # .
-    `)
 basic.forever(function () {
     basic.showLeds(`
-        . # . # .
-        . . . . .
-        # . . . #
-        # . . . #
-        . # # # .
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
         `)
     basic.showLeds(`
-        . # . # .
-        . . . . .
-        # . . . #
-        # . . . #
+        . # . . .
+        . . # . #
         . # # # .
+        # . # . .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . # . # #
+        . . # . .
+        # # . # .
+        . . . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . # .
+        # # . # .
+        . . # . .
+        . # . # #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        # . # . .
+        . # # # .
+        . . # . #
+        . # . . .
         `)
 })
